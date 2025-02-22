1c04794816cd7f7b864019610c31cd91fdd4fad8
beaniecazier <beanieroxiicazier@gmail.com>
2025-02-22 12:22:47 -0800

    1.0.0-a1.3.0 - Add some value converters

---
+++ ValueConverters/ListPronounToIntArrayConverter.cs
@@ -0,0 +1,19 @@
+﻿using BeaniesUtilities.Models.Enum;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeaniesUtilities.SQLDataOperations.ValueConverters;
+
+public class ListPronounToIntArrayConverter : ValueConverter<List<ePronoun>, IEnumerable<int>>
+{
+    public ListPronounToIntArrayConverter() :
+        base(list =>  list.Select(x => (int)x),
+        arr => arr.Select(x => Enum.ToObject(typeof(ePronoun), x)).Cast<ePronoun>().ToList())
+    {
+    }
+}
---
+++ ValueConverters/ListSkillCategoryToIntArrayConverter.cs
@@ -0,0 +1,18 @@
+﻿using BeaniesUtilities.Models.Enum;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeaniesUtilities.SQLDataOperations.ValueConverters;
+
+public class ListSkillCategoryToIntArrayConverter : ValueConverter<List<eSkillCategory>, IEnumerable<int>>
+{
+    public ListSkillCategoryToIntArrayConverter() :
+        base(list => list.Select(x => (int)x),
+        arr => arr.Select(x => Enum.ToObject(typeof(eSkillCategory), x)).Cast<eSkillCategory>().ToList())
+    {
+    }
+}
