4377621f667ded8133e8136bf7b0b1eec347156d
beaniecazier <beanieroxiicazier@gmail.com>
2025-07-30 13:53:43 -0700

    Change types on convert to match nullability rules

--- src/ValueConverters/DictionaryStringStringToJsonConverter.cs
+++ src/ValueConverters/DictionaryStringStringToJsonConverter.cs
@@ -4,23 +4,24 @@ using Microsoft.IdentityModel.Tokens;
 
 namespace Gay.Silverbranch.Api.Sql.ValueConverters;
 
-public class DictionaryStringStringToJsonConverter: ValueConverter<Dictionary<string, string>?, string?>
+public class DictionaryStringStringToJsonConverter: ValueConverter<Dictionary<string, string>, string>
 {
     public DictionaryStringStringToJsonConverter() :
-        base(dict => SerializeDictionary(dict!),
-        str => DeserializeStringToDictionary(str!))
+        base(dict => SerializeDictionary(dict, null),
+        str => DeserializeStringToDictionary(str, null))
     {
     }
 
-    private static Dictionary<string, string>? DeserializeStringToDictionary(string str)
+    private static Dictionary<string, string> DeserializeStringToDictionary(
+        string str,
+        JsonSerializerOptions? options = null)
     {
         if (string.IsNullOrWhiteSpace(str)) return new Dictionary<string, string>();
-        return JsonSerializer.Deserialize<Dictionary<string, string>>(str);
+        return JsonSerializer.Deserialize<Dictionary<string, string>>(str, options)!;
     }
 
-    private static string? SerializeDictionary(Dictionary<string, string> dict)
-    {
-        if (dict.IsNullOrEmpty()) return null;
-        return JsonSerializer.Serialize(dict);
-    }
+    private static string SerializeDictionary(
+        Dictionary<string, string> dict,
+        JsonSerializerOptions? options = null) =>
+    dict.IsNullOrEmpty() ? string.Empty : JsonSerializer.Serialize(dict);
 }
