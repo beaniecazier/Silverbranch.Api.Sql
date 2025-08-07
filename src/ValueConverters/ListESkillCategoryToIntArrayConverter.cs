bd2dde0bf6c1db4bbb72935ed9427734b75bd134
beaniecazier <beanieroxiicazier@gmail.com>
2025-08-07 11:52:00 -0700

    Rename class

---
+++ src/ValueConverters/ListESkillCategoryToIntArrayConverter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+
+using Gay.Silverbranch.Api.Models.Enum.V1;
+
+namespace Gay.Silverbranch.Api.Sql.ValueConverters;
+
+public class ListESkillCategoryToIntArrayConverter : ValueConverter<List<eSkillCategory>, IEnumerable<int>>
+{
+    public ListESkillCategoryToIntArrayConverter() :
+        base(list => ListToIntArr(list),
+        arr => IntArrToList(arr)) { }
+
+    private static IEnumerable<int> ListToIntArr(List<eSkillCategory> list)
+    {
+        return list.Select(x => (int)x);
+    }
+
+    private static List<eSkillCategory> IntArrToList(IEnumerable<int> arr) =>
+        arr.Select(x => Enum.ToObject(typeof(eSkillCategory), x))
+            .Cast<eSkillCategory>()
+            .ToList();
+}
